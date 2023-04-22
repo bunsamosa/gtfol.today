d5919d45cc347f697aafc4ea89991f8ca6a80157
madhukar01 <mholla8@gmail.com>
2023-04-22 08:18:50 -0400

    feat(tweet): embedded tweet component

---
+++ src/lib/components/Tweet.svelte
@@ -0,0 +1,16 @@
+<script>
+	import LazyLoad from '@dimfeld/svelte-lazyload';
+	export let tweetID = '';
+</script>
+
+<svelte:head>
+	<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</svelte:head>
+
+<LazyLoad>
+	<div class="flex justify-center">
+		<blockquote class="twitter-tweet" data-theme="dark">
+			<a class="twitter-tweet" href={`https://twitter.com/x/status/${tweetID}`}>Loading Tweet...</a>
+		</blockquote>
+	</div>
+</LazyLoad>
