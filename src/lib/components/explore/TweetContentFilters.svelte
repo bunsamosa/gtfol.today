ae10a00d1f3473c66191aa545320604faf3993ea
madhukar01 <mholla8@gmail.com>
2023-05-03 12:48:37 -0400

    feat(explore): tweet content filters

---
+++ src/lib/components/explore/TweetContentFilters.svelte
@@ -0,0 +1,13 @@
+<div class="card !bg-transparent p-5">
+	<header class="font-semibold text-lg text-center p-2 pt-0">Tweet content</header>
+	<div class="space-y-2">
+		<label class="flex items-center space-x-2">
+			<input class="checkbox" type="checkbox" name="hasVideo" value={true} />
+			<p>Has video</p>
+		</label>
+		<label class="flex items-center space-x-2">
+			<input class="checkbox" type="checkbox" name="hasPhoto" value={true} />
+			<p>Has photo</p>
+		</label>
+	</div>
+</div>
