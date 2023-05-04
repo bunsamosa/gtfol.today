c335f16a546d0b9d7f8faf4cfb1757c614df8dac
madhukar01 <mholla8@gmail.com>
2023-05-04 09:46:11 -0400

    feat(leaderboard): page layout

---
+++ src/routes/leaderboard/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell
+	class="max-w-7xl m-auto"
+	regionPage="relative"
+	slotPageHeader="lg:sticky lg:top-0 lg:z-10"
+	slotPageContent="overflow-hidden hide-scrollbar"
+>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar background="">
+			<svelte:fragment slot="lead">
+				<a href="/"><img src="/images/gtfol-logo.png" class="max-h-10" alt="GTFOL" /></a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+	import ProjectsList from '$lib/components/leaderboard/ProjectsList.svelte';
+	import ScoreCard from '$lib/components/leaderboard/ScoreCard.svelte';
+</script>
+
+<div class="h-full flex flex-col">
+	<div class="w-full hidden lg:block">
+		<ScoreCard />
+	</div>
+	<ProjectsList />
+</div>
