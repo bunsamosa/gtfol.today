da3e4d4465fe229e8480b74e36676af62a2cd7f3
madhukar01 <mholla8@gmail.com>
2023-05-04 06:57:33 -0400

    feat(leaderboard): house scorecards

---
+++ src/lib/components/leaderboard/ScoreCard.svelte
@@ -0,0 +1,49 @@
+<div class="flex flex-row justify-center">
+	<div
+		class="relative w-64 h-32 bg-cover bg-center rounded-lg shadow-md m-2"
+		style="background-image: url('/images/spectreseek.png')"
+	>
+		<div class="absolute inset-0 bg-gray-900 bg-opacity-50 rounded-lg" />
+		<div class="absolute inset-0 flex items-center justify-center">
+			<div class="text-center">
+				<h2 class="text-3xl font-extrabold sm:text-4xl">10000</h2>
+			</div>
+		</div>
+	</div>
+
+	<div
+		class="relative w-64 bg-cover bg-center rounded-lg shadow-md m-2"
+		style="background-image: url('/images/alterok.png')"
+	>
+		<div class="absolute inset-0 bg-gray-900 bg-opacity-50 rounded-lg" />
+		<div class="absolute inset-0 flex items-center justify-center">
+			<div class="text-center">
+				<h2 class="text-3xl font-extrabold sm:text-4xl">10000</h2>
+			</div>
+		</div>
+	</div>
+
+	<div
+		class="relative w-64 bg-cover bg-center rounded-lg shadow-md m-2"
+		style="background-image: url('/images/gaudmire.png')"
+	>
+		<div class="absolute inset-0 bg-gray-900 bg-opacity-50 rounded-lg" />
+		<div class="absolute inset-0 flex items-center justify-center">
+			<div class="text-center">
+				<h2 class="text-3xl font-extrabold sm:text-4xl">10000</h2>
+			</div>
+		</div>
+	</div>
+
+	<div
+		class="relative w-64 bg-cover bg-center rounded-lg shadow-md m-2"
+		style="background-image: url('/images/erevald.png')"
+	>
+		<div class="absolute inset-0 bg-gray-900 bg-opacity-50 rounded-lg" />
+		<div class="absolute inset-0 flex items-center justify-center">
+			<div class="text-center">
+				<h2 class="text-3xl font-extrabold sm:text-4xl">10000</h2>
+			</div>
+		</div>
+	</div>
+</div>
