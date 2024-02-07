fe253050ea42e0a858fb2cac4086e98a25d503e3
madhukar01 <mholla8@gmail.com>
2024-02-07 12:16:22 -0500

    fix(skeleton): migrate to skeleton v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
+
 
 export default defineConfig({
 	plugins: [sveltekit()]
 });
+
+const config: UserConfig = {
+	plugins: [
+		sveltekit(),
+		purgeCss({
+			safelist: {
+				// any selectors that begin with "hljs-" will not be purged
+				greedy: [/^hljs-/],
+			},
+		}),
+	],
+};
