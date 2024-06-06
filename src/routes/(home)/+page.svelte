7bdc541600e972d2a9d38a23e52d5fffbdac2d39
madhukar01 <mholla8@gmail.com>
2024-06-06 11:49:42 -0400

    fix(home): mobile ui with tabs

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -11,7 +11,7 @@
 
 <!-- Content for large screen only -->
 <div class="hidden lg:block">
-	<div class="flex h-screen">
+	<div class="flex h-screen px-5">
 		<div class="w-3/5 h-full flex flex-col justify-center items-center">
 			<div>
 				<span class="text-7xl font-bold">
@@ -22,10 +22,11 @@
 				<div class="mt-10 flex">
 					<button
 						type="button"
-						class="btn btn-xl variant-filled rounded-full"
+						class="btn btn-lg variant-filled rounded-full"
 					>
-						<span class="px-1">help me launch</span>
-						<Fa icon={faAngleRight} />
+						<span class="inline-flex text-xl font-bold items-center align-baseline">
+							Explore<Fa class="px-1" icon={faAngleRight} />
+						</span>
 					</button>
 				</div>
 			</div>
@@ -60,16 +61,30 @@
 	</div>
 </div>
 
-<!-- Content for mobile only -->
+<!-- Content for mobile screen only -->
 <div class="block lg:hidden">
-	<div class="flex flex-col overflow-hidden hide-scrollbar">
-		<div class="justify-center items-center m-auto">
-			<div class="text-2xl font-bold">top shippers in 24 hours</div>
+	<div class="flex flex-col h-screen">
+		<div class="flex flex-col justify-start h-full overflow-hidden">
+			<TabGroup regionList="sticky top-0 variant-glass-surface">
+				<Tab bind:group={tabSet} name="tab1" value={0}>Last Hour</Tab>
+				<Tab bind:group={tabSet} name="tab2" value={1}>24 Hours</Tab>
+				<Tab bind:group={tabSet} name="tab2" value={2}>7 days</Tab>
+				<Tab bind:group={tabSet} name="tab3" value={3}>Season 5</Tab>
+				<!-- Tab Panels --->
+				<svelte:fragment slot="panel">
+					<Lazy class="px-2">
+						{#if tabSet === 0}
+							<TweetList listType="hour" />
+						{:else if tabSet === 1}
+							<TweetList listType="day" />
+						{:else if tabSet === 2}
+							<TweetList listType="week" />
+						{:else if tabSet === 3}
+							<TweetList listType="season" />
+						{/if}
+					</Lazy>
+				</svelte:fragment>
+			</TabGroup>
 		</div>
-		<Lazy>
-			<div class="flex flex-col justify-start pt-3 overflow-hidden hide-scrollbar" id="mobile">
-				<TweetList listType="day" />
-			</div>
-		</Lazy>
 	</div>
 </div>
